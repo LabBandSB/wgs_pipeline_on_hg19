51ae9cf044bccf2d488510a8e32817ae76615c97
asmo <askhat.molkenov@gmail.com>
2020-05-23 17:16:05 +0600

    v 0.1.2 - update

--- environment.sh
+++ environment.sh
@@ -3,4 +3,5 @@ conda install -c bioconda git python fastqc bwa samtools picard gatk perl-vcftoo
 # conda install -c bioconda https://anaconda.org/bioconda/samtools/1.2/download/linux-64/samtools-1.2-2.tar.bz2
 
 ref=""
-bwa index $ref
+bwa index ${ref}
+samtools faidx ${ref}
